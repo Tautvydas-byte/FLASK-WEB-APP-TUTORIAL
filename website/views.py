5c7dca520c711bc68eb1311e340f92dc724a8a54
Tautvydas <tautvydas.kvietkauskas@gmail.com>
2021-05-05 00:06:28 +0300

    Adding User Note

--- website/views.py
+++ website/views.py
@@ -1,9 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
+
 views = Blueprint('views', __name__)
 
 
-@views.route('/')  # /home
+@views.route('/', methods=['GET', 'POST'])  # /home
 @login_required  # cannot go to home page without login
 def home():
-    return render_template("home.html", user=current_user)# user=current_user check if this is autheticated
+    if request.method == 'POST':
+        note = request.form.get('note')
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    # user=current_user check if this is autheticated
+    return render_template("home.html", user=current_user)
